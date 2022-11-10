fff13ee878b485b71f751ebdef4f8a71cc1b218e
hachadorian <hachadorian98@gmail.com>
2022-11-10 15:07:30 -0500

    refactor lambda function return

--- increment/app.py
+++ increment/app.py
@@ -1,6 +1,5 @@
-import json
-
 import boto3
+import simplejson as json
 
 
 def lambda_handler(event, context):
@@ -11,9 +10,10 @@ def lambda_handler(event, context):
             'id': '0'
         },
         ExpressionAttributeValues={':val': 1},
-        UpdateExpression="set visitor_count = visitor_count + :val"
+        UpdateExpression="set visitor_count = visitor_count + :val",
+        ReturnValues="UPDATED_NEW"
     )
-    
+
     return {
       'statusCode': 200,
       'body': json.dumps(data),
